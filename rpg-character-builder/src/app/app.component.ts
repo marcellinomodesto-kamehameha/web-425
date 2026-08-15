6015b90f5a801a45e14301a388ef6eacee3b3bb7
Marcellino <marcellinomodesto@gmail.com>
2026-08-15 14:46:21 -0700

    feat: Completed week 1 assignment

--- rpg-character-builder/src/app/app.component.ts
+++ rpg-character-builder/src/app/app.component.ts
@@ -5,7 +5,22 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
-    <router-outlet />
+    <header>
+      <h1>RPG Character Builder</h1>
+
+      <nav>
+        <a href="/">Home</a>
+         <a href="/">About</a>
+      </nav>
+    </header>
+
+    <main>
+      <router-outlet />
+    </main>
+
+    <footer>
+      <p>WEB 425 · RPG Character Builder</p>
+    </footer>
   `
 })
 export class AppComponent {}
